73c189f17de0f27d4e6dc5263d72d74a7bd4a2c8
Justin Endler <justin.endler@gmail.com>
2017-12-30 19:21:21 -0500

    Implement pre-redux course example with minor tweaks.

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+      </div>
+    );
+  }
+
+  onInputChange(e) {
+    const term = e.target.value;
+
+    this.setState({
+      term
+    });
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+import ytApiKey from './config.js';
+
+const DEFAULT_TERM = 'zappa';
+const DEBOUNCE_INTERVAL = 300;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch(DEFAULT_TERM);
+  }
+
+  videoSearch(term) {
+    YTSearch({
+      key: ytApiKey,
+      term
+    }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    // reduce the frequency of the search call
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, DEBOUNCE_INTERVAL);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
